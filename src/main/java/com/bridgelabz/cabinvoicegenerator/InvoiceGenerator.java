f9e2245e9858f080576693f7ac4637fa59373126
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-18 10:42:58 -0800

    Added: Calculating fare by taking distance and time

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -1,9 +1,12 @@
 package com.bridgelabz.cabinvoicegenerator;
 
 public class InvoiceGenerator {
+	private static final double COST_PER_KM = 10;
+	private static final double COST_PER_MIN = 1;
+	private static final double MIN_FARE = 5.0;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Cab Invoice Generator");
+	public double calculateFare(double distance, int time) {
+		double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+		return Math.max(totalFare, MIN_FARE);
 	}
-
 }
