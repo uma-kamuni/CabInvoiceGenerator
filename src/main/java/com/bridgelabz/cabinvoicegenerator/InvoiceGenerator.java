e5465a1a6eb096cd2d3a6cbd1fc4f6a42bb313a2
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-18 10:29:47 -0800

    Welcome to Cab Invoice Generator Program

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class InvoiceGenerator {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Cab Invoice Generator");
+	}
+
+}
