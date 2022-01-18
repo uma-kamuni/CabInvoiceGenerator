b00a4e3f6443236c6c0b8c542d3e8900ffa83eee
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-18 10:49:16 -0800

    Added: Class to hold details of each Ride

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class Ride {
+	
+	public double distance;
+	public int time;
+	
+	Ride(double distance, int time){
+		this.distance = distance;
+		this.time = time;
+	}
+}
