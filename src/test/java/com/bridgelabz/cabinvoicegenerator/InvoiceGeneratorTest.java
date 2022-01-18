086e991c896f4cadfea1c68398dc4367be6ab56b
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-18 10:50:59 -0800

    Refactored: testing method to calculate total fare for multiple rides

--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -4,23 +4,34 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class InvoiceGeneratorTest {
-
+	
+	public static InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+	
 	@Test
 	public void givenDistanceAndTime_ShouldReturnTotalFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-		double distance = 2.0;
+		
+		double distance  = 2.0;
 		int time = 5;
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		Assert.assertEquals(25, fare, 0.0);
-
+		
 	}
-
+	
 	@Test
 	public void givenLessDistanceAndTime_ShouldReturnMinFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+
 		double distance = 0.1;
 		int time = 1;
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		Assert.assertEquals(5, fare, 0.0);
+	}
+	
+	@Test
+	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+
+		Ride[] rides = {new Ride(2.0, 5), 
+						new Ride(0.1, 1)};
+		double totalFare = invoiceGenerator.calculateFare(rides);
+		Assert.assertEquals(30, totalFare, 0.0);
 	}
 }
