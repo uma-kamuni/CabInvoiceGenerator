cfc7c1ed99c918de07bb9a133e103f376eb36cf9
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-18 10:47:46 -0800

    Refactored: Method to calculate total fare for multiple rides

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -1,12 +1,25 @@
 package com.bridgelabz.cabinvoicegenerator;
 
 public class InvoiceGenerator {
-	private static final double COST_PER_KM = 10;
-	private static final double COST_PER_MIN = 1;
-	private static final double MIN_FARE = 5.0;
+	
+	private static final double MINIMUM_COST_PER_KILOMETER = 10;
+	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_FARE = 5;
 
 	public double calculateFare(double distance, int time) {
-		double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
-		return Math.max(totalFare, MIN_FARE);
+		
+		double totalFare = distance*MINIMUM_COST_PER_KILOMETER + time*COST_PER_TIME;
+		if(totalFare<MINIMUM_FARE)
+			return MINIMUM_FARE;
+		return totalFare;
+	}
+
+	public double calculateFare(Ride[] rides) {
+		
+		double totalFare = 0;
+		for(Ride ride : rides) {
+			totalFare = totalFare + this.calculateFare(ride.distance, ride.time);
+		}
+		return totalFare;
 	}
 }
