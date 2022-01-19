572b07252f8aa9472b571752aa33bc50f8efa811
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-18 20:10:53 -0800

    Refactored: Changed parameters for the methods to include type

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorIF.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorIF.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public interface InvoiceGeneratorIF {
 	
-	public double calculateFare(double distance, int time);
-	public double calculateFare(Ride[] rides);
-	public InvoiceSummary calculateFareReturnObject(ArrayList<Ride> rides);
+	public double calculateFare(double distance, int time, String type);
+	public double calculateFare(Ride[] rides, String type);
+	public InvoiceSummary calculateFareReturnObject(ArrayList<Ride> rides, String type);
 	
 }
