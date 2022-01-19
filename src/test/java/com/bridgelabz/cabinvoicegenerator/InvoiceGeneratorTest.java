1ef51cd3aeab7e6f052ac17996cc3c5ba75ba6f5
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-18 19:49:23 -0800

    Refactored: Tested invoice summary of perticular user

--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.cabinvoicegenerator;
 
+import java.util.ArrayList;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -38,11 +39,31 @@ public class InvoiceGeneratorTest {
 	@Test
 	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
 
-		Ride[] rides = {new Ride(2.0, 5), 
-						new Ride(0.1, 1)};
+		ArrayList<Ride> rides = new ArrayList<Ride>();
+		rides.add(new Ride(2.0, 5));
+		rides.add(new Ride(0.1, 1));
 		InvoiceSummary summary = invoiceGenerator.calculateFareReturnObject(rides);
 		InvoiceSummary expectedSummary = new InvoiceSummary(2, 30);
 		if(expectedSummary.getAverageFare() == summary.getAverageFare() && expectedSummary.getNumberOfRides() == summary.getNumberOfRides() && expectedSummary.getTotalFare() == summary.getTotalFare())
+			Assert.assertEquals(1, 1);
+	}
+	
+	@Test
+	public void givenUserId_ShouldReturnInvoiceSummary() {
+		
+		String userId = "User1";
+		InvoiceService invoiceService = new InvoiceService();
+		
+		ArrayList<Ride> rides = new ArrayList<Ride>();
+		rides.add(new Ride(2.0, 5));
+		rides.add(new Ride(0.1, 1));
+		
+		invoiceService.addRide(userId, rides);
+		ArrayList<Ride> listOfRides = invoiceService.getRides(userId);
+		
+		InvoiceSummary summaryForUser1 = invoiceGenerator.calculateFareReturnObject(listOfRides);
+		InvoiceSummary expectedSummary = new InvoiceSummary(2, 30);
+		if(expectedSummary.getAverageFare() == summaryForUser1.getAverageFare() && expectedSummary.getNumberOfRides() == summaryForUser1.getNumberOfRides() && expectedSummary.getTotalFare() == summaryForUser1.getTotalFare())
 			Assert.assertEquals(1, 1);
 	}
 }
